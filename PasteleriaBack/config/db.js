1bc2b88c8ffd19c3732eb13e87546ed39f153a95
ArelyLopezS <arelialopezs@gmail.com>
2024-11-24 14:39:33 -0600

    Base de Datos

--- PasteleriaBack/config/db.js
+++ PasteleriaBack/config/db.js
@@ -1,15 +1,21 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456789',
-    database: 'dulce_arelys'
-})
+    host: 'virtualazure.database.windows.net',  // Reemplaza con tu servidor en Azure
+    user: 'andres',                // Reemplaza con tu usuario de Azure
+    password: 'Royer280504+',                     // Reemplaza con tu contraseña
+    database: 'dulce_arelys',                      // El nombre de la base de datos en Azure
+    ssl: {
+        ca: fs.readFileSync('/path-to-ca-cert.pem')  // Opcional, si usas SSL para la conexión (puedes omitirlo si no es necesario)
+    }
+});
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log('Conexion exitosa a la base de datos')
+    if (err) {
+        console.error('Error al conectar: ', err);
+        throw err;
+    }
+    console.log('Conexión exitosa a la base de datos en Azure');
 });
 
 module.exports = connection;
